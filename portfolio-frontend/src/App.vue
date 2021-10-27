95c9b129e83f1ca6b52aa0585f116cf4a9310f1a
Jake Steele <jake.steele54@gmail.com>
2021-10-27 09:36:08 -0400

    Fixed linting errors

--- portfolio-frontend/src/App.vue
+++ portfolio-frontend/src/App.vue
@@ -4,15 +4,6 @@
   </div>
 </template>
 
-<script>
-import NavBar from '../src/components/NavBar.vue'
-export default {
-  components:{
-    NavBar
-  }
-}
-</script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
