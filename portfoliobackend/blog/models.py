f9727182b0925b744c47a76a5a961353ab03ba32
Jake Steele <jake.steele54@gmail.com>
2022-02-03 20:19:11 -0500

    Fixed Tag Model

--- portfoliobackend/blog/models.py
+++ portfoliobackend/blog/models.py
@@ -39,7 +39,7 @@ class Category(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True, null=False)
-    posts = models.ManyToManyField(Post, related_name='tags')
+    posts = models.ManyToManyField(Post, related_name='tags', blank=True)
 
     def __str__(self):
         return self.name
